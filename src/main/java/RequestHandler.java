b8a0ef2fc94c51e937d9b6940c1b317b15979e8d
David Lu <dvdlu13@gmail.com>
2016-04-11 16:38:17 -0700

    Transform output to JSON

--- src/main/java/RequestHandler.java
+++ src/main/java/RequestHandler.java
@@ -1,7 +1,24 @@
 import static spark.Spark.*;
 
+import java.util.List;
+import java.util.Map;
+
 public class RequestHandler {
+	
 	public static void main(String[] args) {
-		get("/hello", (req, res) -> "Hello World");
+		SPARQLQueryRunner queryRunner = new SPARQLQueryRunner();
+		
+		get("/pokemon/:id", (req, res) -> {
+//			StringBuilder output = new StringBuilder();
+//			List<Map<String, String>> results = queryRunner.findPokemonById(Integer.parseInt(req.params(":id")));
+//			for (Map<String, String> result : results) {
+//				for (String attr : result.keySet()) {
+//					output.append(attr + ": " + result.get(attr) + "\n");
+//				}
+//			}
+//			return output.toString();
+//		});
+			return queryRunner.findPokemonById(Integer.parseInt(req.params(":id")));
+		}, new JsonTransformer());
 	}
 }
--- src/main/java/SPARQLQueryRunner.java
+++ src/main/java/SPARQLQueryRunner.java
@@ -3,11 +3,17 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
 import org.apache.jena.query.ResultSetFormatter;
 import org.apache.jena.rdf.model.Model;
@@ -15,7 +21,7 @@ import org.apache.jena.rdf.model.ModelFactory;
 
 public class SPARQLQueryRunner {
 
-	public ResultSet findPokemonByIdQuery(int id) throws IOException {
+	public List<Map<String, String>> findPokemonById(int id) throws IOException {
 		String queryString = 
 			"PREFIX pkm: <http://pokedex.dataincubator.org/pkm/> " +
 			"SELECT ?name ?color ?attack " +
@@ -24,11 +30,10 @@ public class SPARQLQueryRunner {
 			"      <http://pokedex.dataincubator.org/pokemon/" + id + "> pkm:colour ?color. " +
 			"      <http://pokedex.dataincubator.org/pokemon/" + id + "> pkm:baseAttack ?attack. " +
 			"}";
-		
 		return runSPARQLQuery(queryString);
 	}
 	
-	public static ResultSet runSPARQLQuery(String queryString) throws IOException {
+	public static List<Map<String, String>> runSPARQLQuery(String queryString) throws IOException {
 		InputStream in = new FileInputStream(new File("Pokedex.rdf"));
 		Model model = ModelFactory.createDefaultModel();
 		model.read(in, null, "N-TRIPLES");
@@ -37,21 +42,30 @@ public class SPARQLQueryRunner {
 		System.out.println(queryString);
 		Query query = QueryFactory.create(queryString);
 
-		// Execute the query and obtain results
 		QueryExecution qe = QueryExecutionFactory.create(query, model);
 		ResultSet results = qe.execSelect();
 
-		// Output query results	
-		ResultSetFormatter.out(System.out, results, query);
+//		ResultSetFormatter.out(System.out, results, query);
 
-		// Important - free up resources used running the query
+		List<Map<String, String>> resultsList = new LinkedList<Map<String, String>>();
+		while (results.hasNext()) {
+			Map<String, String> map = new HashMap<String, String>();
+			QuerySolution result = results.next();
+			Iterator<String> varNames = result.varNames();
+			while (varNames.hasNext()){
+				String varName = varNames.next();
+				map.put(varName, result.getLiteral(varName).getString());
+			}
+			resultsList.add(map);
+		}
+		
 		qe.close();
 		
-		return results;
+		return resultsList;
 	}
 	
 	public static void main(String[] args) throws IOException {
 		SPARQLQueryRunner queryRunner = new SPARQLQueryRunner();
-		queryRunner.findPokemonByIdQuery(1);
+		queryRunner.findPokemonById(1);
 	}
 }
